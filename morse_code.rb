2d18255c262ebb76bcb509d80282eccee5c8d0f3
abel-tefera <abeltefera16@gmail.com>
2023-06-16 11:20:48 +0300

    decode char and decode word done

---
+++ morse_code.rb
@@ -0,0 +1,42 @@
+MORSE_CODE_DICT = {
+  '.-' => 'a',
+  '-...' => 'b',
+  '-.-.' => 'c',
+  '-..' => 'd',
+  '.' => 'e',
+  '..-.' => 'f',
+  '--.' => 'g',
+  '....' => 'h',
+  '..' => 'i',
+  '.---' => 'j',
+  '-.-' => 'k',
+  '.-..' => 'l',
+  '--' => 'm',
+  '-.' => 'n',
+  '---' => 'o',
+  '.--.' => 'p',
+  '--.-' => 'q',
+  '.-.' => 'r',
+  '...' => 's',
+  '-' => 't',
+  '..-' => 'u',
+  '...-' => 'v',
+  '.--' => 'w',
+  '-..-' => 'x',
+  '-.--' => 'y',
+  '--..' => 'z'
+}.freeze
+
+def decode_char(char)
+  MORSE_CODE_DICT[char].upcase
+end
+
+def decode_word(word)
+  splited = word.split
+  decoded_word = ''
+  splited.each do |c|
+    decoded_word += decode_char(c)
+  end
+  decoded_word
+end
+
